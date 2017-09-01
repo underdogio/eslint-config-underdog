07250a3c74e0be03c627aae98144fdeb13b020f6
Mario Pabon <me@mariopabon.com>
2017-09-01 11:49:48 -0400

    Combine es6 and react configs

--- es6.js
+++ es6.js
@@ -1,10 +1,16 @@
 module.exports = {
-  extends: './index.js',
+  extends: ['./index.js', 'plugin:react/recommended'],
   env: {
     es6: true
   },
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
     ecmaVersion: 6,
     sourceType: 'module'
-  }
+  },
+  plugins: [
+    "react"
+  ]
 };
